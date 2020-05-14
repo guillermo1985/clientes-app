037f2f7b8a9602cf3c06651cf854d2b4187044a0
guillermo <guillermorof85@gmail.com>
2020-05-14 17:40:56 -0500

    Se crea Clase de servicio ClienteService y se inyecta dependencias

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
+import { ClienteService } from './cliente.service';
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -9,10 +10,9 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit() {
-    this.clientes = CLIENTES;
+    this.clientes = this.clienteService.getClientes();
   }
-
 }
