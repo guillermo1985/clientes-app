5be48328489407e2d6a59a4c0b6d757b4604bb68
guillermo <guillermorof85@gmail.com>
2020-05-14 17:19:31 -0500

    Se agrega clase clientes y archivo json para visualizar clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Guillermo', apellido: 'Romero', email: 'guillermor@gmail.com', createAt: '14-05-2020'},
+    {id: 2, nombre: 'Juan', apellido: 'Flores', email: 'juanf@gmail.com', createAt: '10-05-2020'},
+    {id: 3, nombre: 'Francisco', apellido: 'Gutierrez', email: 'Franciscog@gmail.com', createAt: '12-05-2020'},
+    {id: 4, nombre: 'Rogelio', apellido: 'Juarez', email: 'rogelioj@gmail.com', createAt: '5-05-2020'},
+    {id: 5, nombre: 'Carmen', apellido: 'Martinez', email: 'carmenm@gmail.com', createAt: '7-05-2020'},
+    {id: 6, nombre: 'Alejandra', apellido: 'Soto', email: 'alejandras@gmail.com', createAt: '20-05-2020'},
+    {id: 7, nombre: 'Mariana', apellido: 'Hernandez', email: 'marianah@gmail.com', createAt: '14-06-2020'},
+    {id: 8, nombre: 'Carlos', apellido: 'Ruiz', email: 'carlosr@gmail.com', createAt: '24-06-2020'},
+    {id: 9, nombre: 'Federico', apellido: 'Gallegos', email: 'federicog@gmail.com', createAt: '31-04-2020'},
+    {id: 10, nombre: 'Jaime', apellido: 'Avalos', email: 'jaimea@gmail.com', createAt: '30-06-2020'},
+    {id: 11, nombre: 'Anastasia', apellido: 'Carrasco', email: 'anastasiac@gmail.com', createAt: '11-03-2020'}
+]
