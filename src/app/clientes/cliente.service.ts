df6a46942a345461cfadad4b74155cf502c8a004
guillermo <guillermorof85@gmail.com>
2020-05-15 13:59:48 -0500

    Implementa clase Observable y rutas de navegacion

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+import { Observable,of } from 'rxjs';
 
 @Injectable()
  
@@ -8,7 +9,7 @@ export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[]{
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]>{
+    return of(CLIENTES);
   }
 }
